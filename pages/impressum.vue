ac26a8874ff93fa5e1d1f5760dd1e9e8176ace5c
EricHier <19eric03@gmail.com>
2020-05-09 18:12:02 +0200

    fixed impressum layout

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,60 +1,60 @@
 <template>
-    <div class="container mx-auto p-10 text-justify">
-        <h1 class="text-3xl font-bold">Impressum</h1>
+  <div class="px-8 py-10 w-full animated fadeIn faster">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-bold">Impressum</h1>
 
-        <h2 class="mt-8 text-2xl font-bold">Angaben gemäß 5 TMG</h2>
-        <p class="mt-4">
-            Eric Feltgen<br/>
-            Heerlenerstr. 16<br/>
-            52074 Aachen
-        </p>
+      <h2 class="mt-8 text-2xl font-bold">Angaben gemäß 5 TMG</h2>
+      <p class="mt-4">
+        Eric Feltgen<br/>
+        Heerlenerstr. 16<br/>
+        52074 Aachen
+      </p>
 
-        <h2 class="mt-8 text-2xl font-bold">Kontakt</h2>
-        <p class="mt-4">
-            Telefon: 0241 55705543<br/>
-            E-Mail: developer.erichier@gmail.com
-        </p>
+      <h2 class="mt-8 text-2xl font-bold">Kontakt</h2>
+      <p class="mt-4">
+        Telefon: 0241 55705543<br/>
+        E-Mail: developer.erichier@gmail.com
+      </p>
 
-        <h3 class="mt-8 text-2xl font-bold">Haftung f&uuml;r Inhalte</h3>
-        <p class="mt-4">
-            Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten
-            nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter
-            jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder
-            nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p>
-        <p class="mt-4">
-            Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen
-            bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der
-            Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden
-            Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-        </p>
-        <h3 class="mt-8 text-2xl font-bold">Haftung f&uuml;r Links</h3>
-        <p class="mt-4">
-            Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
-            Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die
-            Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die
-            verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft.
-            Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
-        </p>
-        <p class="mt-4">
-            Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
-            Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links
-            umgehend entfernen.
-        </p>
-        <h3 class="mt-8 text-2xl font-bold">Urheberrecht</h3>
-        <p class="mt-4">
-            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
-            Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der Verwertung au&szlig;erhalb
-            der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
-            Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r den privaten, nicht kommerziellen Gebrauch
-            gestattet.
-        </p>
-        <p class="mt-4">
-            Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter
-            beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
-            Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von
-            Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
-        </p>
-
-        <Particles/>
+      <h3 class="mt-8 text-2xl font-bold">Haftung f&uuml;r Inhalte</h3>
+      <p class="mt-4">
+        Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten
+        nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter
+        jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder
+        nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p>
+      <p class="mt-4">
+        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen
+        bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der
+        Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden
+        Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+      </p>
+      <h3 class="mt-8 text-2xl font-bold">Haftung f&uuml;r Links</h3>
+      <p class="mt-4">
+        Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
+        Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die
+        Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die
+        verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft.
+        Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+      </p>
+      <p class="mt-4">
+        Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
+        Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+        umgehend entfernen.
+      </p>
+      <h3 class="mt-8 text-2xl font-bold">Urheberrecht</h3>
+      <p class="mt-4">
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
+        Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der Verwertung au&szlig;erhalb
+        der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
+        Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r den privaten, nicht kommerziellen Gebrauch
+        gestattet.
+      </p>
+      <p class="mt-4">
+        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter
+        beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+        Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von
+        Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+      </p>
     </div>
+  </div>
 </template>
